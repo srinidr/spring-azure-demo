b937e7dd58ab777b97748adf145b27ed518adc9d
srinidr <srinidr@yahoo.com>
2022-12-10 23:39:47 +0530

    first commit

---
+++ src/main/java/com/sitec/salesdemo/SalesDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sitec.salesdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sitec/salesdemo/SalesDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sitec.salesdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
