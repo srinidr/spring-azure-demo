043fd33c80d3342fd08c3631cb9380b086a725ac
srinidr <srinidr@yahoo.com>
2022-12-11 02:07:02 +0530

    moved to respective modules

--- src/main/java/com/sitec/salesdemo/Demo.java
+++ src/main/java/com/sitec/salesdemo/Demo.java
@@ -4,6 +4,9 @@
  */
 package com.sitec.salesdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.JdbcTemplateAutoConfiguration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,8 +18,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class Demo {
     
+    @Autowired
+    JdbcTemplate jd;
+    
+    
     @RequestMapping("/")
     @ResponseBody  public String hello(){
+        jd.query("select * from cust", (rs, rowNum) -> {
+            System.out.println(rs.getString("name"));
+            return  null;
+                   
+        });
         return "Hi Srini";
     }
     
